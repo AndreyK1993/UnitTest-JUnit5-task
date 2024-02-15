28cc40804823151637f166c7e8922c4e58a81cf6
AndreyK1993 <andrey.kurilenko27@gmail.com>
2024-02-16 00:07:11 +0200

    added tests

--- src/test/java/app/utils/AppValidatorTest.java
+++ src/test/java/app/utils/AppValidatorTest.java
@@ -1,14 +1,40 @@
 package app.utils;
 
-import static org.junit.jupiter.api.Assertions.*;
+import app.exceptions.TemperatureException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AppValidatorTest {
 
-    @org.junit.jupiter.api.BeforeEach
+    private AppValidator validator;
+
+    @BeforeEach
     void setUp() {
+        validator = new AppValidator();
     }
 
-    @org.junit.jupiter.api.AfterEach
+    @Test
+    @DisplayName("Test if temperature is in bounds.")
+    void testIfTemperatureIsInBounds() throws TemperatureException {
+        assertEquals("Temperature is OK. Data can be obtained.",
+                validator.validateTemperature(15));
+    }
+
+    @Test
+    @DisplayName("Test if invalid temperature throws exception.")
+    void testIfInvalidTemperatureThrowsException() {
+        TemperatureException exception = assertThrows(TemperatureException.class, () ->
+                validator.validateTemperature(1000));
+        assertEquals("Invalid temperature.", exception.getMessage(), "Exception message is incorrect.");
+    }
+
+    @AfterEach
     void tearDown() {
+        validator = null;
     }
 }
