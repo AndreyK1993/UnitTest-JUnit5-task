4bec1c7a368b58da8130607e5228115443992150
AndreyK1993 <andrey.kurilenko27@gmail.com>
2024-02-15 23:53:19 +0200

    added test junit

---
+++ src/test/java/app/utils/AppValidatorTest.java
@@ -0,0 +1,14 @@
+package app.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppValidatorTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
